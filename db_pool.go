280982c81ef21563f7027ff33886c3fc3b2fcce0
Magnus Larsen <magnushlarsen01@protonmail.com>
2026-03-15 14:57:00 +0100

    update

--- db_pool.go
+++ db_pool.go
@@ -27,7 +27,7 @@ func async(fn *C.zval, args *C.zend_array) (unsafe.Pointer) {
 }
 
 //export_php:function await(string $key): mixed;
-func await(key C.zend_string) (unsafe.Pointer) {
+func await(key *C.zend_string) (unsafe.Pointer) {
 	go_key := frankenphp.GoString(unsafe.Pointer(key));
 	channel := callbacks[go_key]
 	res := <-channel;
