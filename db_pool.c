f3ee86371880daaf671d45452b56d362e9a6d1f8
Magnus Larsen <magnushlarsen01@protonmail.com>
2026-03-15 19:27:19 +0100

    update

--- db_pool.c
+++ db_pool.c
@@ -55,7 +55,6 @@ PHP_FUNCTION(await)
     ZEND_PARSE_PARAMETERS_START(1, 1)
         Z_PARAM_STR(key)
     ZEND_PARSE_PARAMETERS_END();
-    GoInterface tmp = go_await(key);
-    zval *result = PHPValue(tmp);
+    zval *result = go_await(key);
 }
 
